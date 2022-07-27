3a50286e43a9de16d6b1258f2cb68ab7f2b53849
dolgogae <ohsh0731@gmail.com>
2022-07-27 23:02:00 +0900

    220727

--- programmers/python/신규아이디추천.py
+++ programmers/python/신규아이디추천.py
@@ -1,4 +1,27 @@
 def solution(new_id):
-    answer = ''
+    answer =''
+    new_id = new_id.lower()
+    for c in new_id:
+        if c.isdigit() or c.isalpha() or c in ['_','-','.']:
+            answer += c
+
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+
+    if answer[0] == '.':
+        answer = answer[1:] if len(answer) > 1 else '.'
+    if answer[-1] == '.':
+        answer = answer[:-1]
+
+    if len(answer) == 0:
+        answer = 'a'
+    answer = answer[0:15]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+
+    while len(answer) < 3:
+        answer += answer[-1]
 
     return answer
+
+print(solution("z-+.^."))
